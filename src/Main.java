239169b327f7ce76c4ab035f9b146f319fb82e16
UsrMakson <166782494+UsrMakson@users.noreply.github.com>
2026-01-14 19:50:03 +0500

    update

--- src/Main.java
+++ src/Main.java
@@ -10,53 +10,19 @@ public class Main {
         System.out.print(": ");
         int choose_task = scanner.nextInt();
         switch (choose_task) {
-            case 1: {
+            case 1:{
                 int switch_choose;
                 System.out.println("-----------------------------------------------------------------------------------------");
-                System.out.println("Выберите задачу(2/4/6/8/10)");
+                System.out.println("Выберите задачу(4/10)");
                 System.out.print(": ");
                 switch_choose = scanner.nextInt();
                 switch (switch_choose) {
-                    case 2: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
-                        z = funk.sumLastNums(x);
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " + z);
-                        break;
-
-                    }
                     case 4: {
                         int a;
                         System.out.print("введите a: ");
                         a = scanner.nextInt();
                         System.out.println("-----------------------------------------------------------------------------------------");
                         System.out.println("Результат: " + funk.isPositive(a));
-                        break;
-                    }
-                    case 6: {
-                        char x;
-                        String s;
-                        boolean z;
-                        System.out.print("введите a: ");
-                        s = scanner.next();
-                        x = s.charAt(0);
-                        z = funk.isUpperCase(x);
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " + z);
-                        break;
-                    }
-                    case 8: {
-                        int a, b;
-                        boolean z;
-                        System.out.print("введите a: ");
-                        a = scanner.nextInt();
-                        System.out.print("введите b: ");
-                        b = scanner.nextInt();
-                        z = funk.isDivisor(a, b);
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " + z);
                         break;
                     }
                     case 10: {
@@ -76,158 +42,44 @@ public class Main {
                     }
                 }
             }// Задача 1
-            case 2: {
-                int switch_choose;
+            case 2:{
                 System.out.println("-----------------------------------------------------------------------------------------");
-                System.out.println("Выберите задачу(2/4/6/8/10)");
-                System.out.print(": ");
-                switch_choose = scanner.nextInt();
-                switch (switch_choose) {
-                    case 2: {
-                        int x, y, z;
-                        boolean s = false;
-                        System.out.println("введите x");
-                        x = scanner.nextInt();
-                        System.out.println("введите y");
-                        y = scanner.nextInt();
-                        System.out.println("введите z");
-                        z = scanner.nextInt();
-                        s = funk.sum3(x,y,z);
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " + s);
-                        break;
-                    }
-                    case 4: {
-                        int a, b;
-                        System.out.print("введите x: ");
-                        a = scanner.nextInt();
-                        System.out.print("введите y: ");
-                        b = scanner.nextInt();
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " + funk.sum2(a, b));
-                        break;
-                    }
-                    case 6: {
-                        int x;
-                        String z;
-                        System.out.print("введите x: ");
-                        x = scanner.nextInt();
-                        z = funk.age(x);
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " + z);
-                        break;
-                    }
-                    case 8: {
-                        int a, b, c;
-                        boolean z;
-                        System.out.print("введите a: ");
-                        a = scanner.nextInt();
-                        System.out.print("введите b: ");
-                        b = scanner.nextInt();
-                        System.out.print("введите c: ");
-                        c = scanner.nextInt();
-                        z = funk.sum3(a, b, c);
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " + z);
-                        break;
-                    }
-                    case 10: {
-                        int temp, res = 0;
-                        for (int i = 0; i < 5; i++) {
-                            System.out.print("Введите число " + (i + 1) + ": ");
-                            temp = scanner.nextInt();
-                            res = funk.lastNumSum(temp, res);
-                        }
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Итого: " + res);
-                        break;
-                    }
-                    default: {
-                        System.out.println("Нет задачи в задании");
-                        break;
-                    }
-                }
+
+                break;
+            }
             }// Задача 2
-            case 3: {
-                int switch_choose;
-                System.out.println("-----------------------------------------------------------------------------------------");
-                System.out.println("Выберите задачу(2/4/6/8/10)");
-                System.out.print(": ");
-                switch_choose = scanner.nextInt();
-                switch (switch_choose) {
-                    case 2: {
-                        int x, y, z;
-                        boolean s = false;
-                        System.out.println("введите x");
-                        x = scanner.nextInt();
-                        System.out.println("введите y");
-                        y = scanner.nextInt();
-                        System.out.println("введите z");
-                        z = scanner.nextInt();
-                        s = funk.sum3(x,y,z);
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " + s);
-                        break;
-                    }
-                    case 4: {
-                        int a, b;
-                        System.out.print("введите x: ");
-                        a = scanner.nextInt();
-                        System.out.print("введите y: ");
-                        b = scanner.nextInt();
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " + funk.sum2(a, b));
-                        break;
-                    }
-                    case 6: {
-                        int x;
-                        String z;
-                        System.out.print("введите x: ");
-                        x = scanner.nextInt();
-                        z = funk.age(x);
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " + z);
-                        break;
-                    }
-                    case 8: {
-                        int a, b, c;
-                        boolean z;
-                        System.out.print("введите a: ");
-                        a = scanner.nextInt();
-                        System.out.print("введите b: ");
-                        b = scanner.nextInt();
-                        System.out.print("введите c: ");
-                        c = scanner.nextInt();
-                        z = funk.sum3(a, b, c);
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " + z);
-                        break;
-                    }
-                    case 10: {
-                        int temp, res = 0;
-                        for (int i = 0; i < 5; i++) {
-                            System.out.print("Введите число " + (i + 1) + ": ");
-                            temp = scanner.nextInt();
-                            res = funk.lastNumSum(temp, res);
-                        }
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Итого: " + res);
-                        break;
-                    }
-                    default: {
-                        System.out.println("Нет задачи в задании");
-                        break;
-                    }
-                }
+            case 3:{
+            System.out.println("-----------------------------------------------------------------------------------------");
+
+            break;
+        }
             }// Задача 3
-            case 4: {
+            case 4:
+        System.out.println("-----------------------------------------------------------------------------------------");
+
+        break;
+
+            }// Задача 4
+            case 5:{
+
+                    System.out.println("-----------------------------------------------------------------------------------------");
+
+                                break;
+                                        }// Задача 5
+            case 6:{
+
+                System.out.println("-----------------------------------------------------------------------------------------");
+
+                break;
+            }// Задача 6
+            case 7:{
                 int switch_choose;
                 System.out.println("-----------------------------------------------------------------------------------------");
-                System.out.println("Выберите задачу(2/4/6/8/10)");
+                System.out.println("Выберите задачу(1/2/3)");
                 System.out.print(": ");
                 switch_choose = scanner.nextInt();
                 switch (switch_choose) {
-                    case 2: {
+                    case 1: {
                         int x, y, z;
                         boolean s = false;
                         System.out.println("введите x");
@@ -241,7 +93,7 @@ public class Main {
                         System.out.println("Результат: " + s);
                         break;
                     }
-                    case 4: {
+                    case 2: {
                         int a, b;
                         System.out.print("введите x: ");
                         a = scanner.nextInt();
@@ -251,7 +103,7 @@ public class Main {
                         System.out.println("Результат: " + funk.sum2(a, b));
                         break;
                     }
-                    case 6: {
+                    case 3: {
                         int x;
                         String z;
                         System.out.print("введите x: ");
@@ -261,37 +113,17 @@ public class Main {
                         System.out.println("Результат: " + z);
                         break;
                     }
-                    case 8: {
-                        int a, b, c;
-                        boolean z;
-                        System.out.print("введите a: ");
-                        a = scanner.nextInt();
-                        System.out.print("введите b: ");
-                        b = scanner.nextInt();
-                        System.out.print("введите c: ");
-                        c = scanner.nextInt();
-                        z = funk.sum3(a, b, c);
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " + z);
-                        break;
-                    }
-                    case 10: {
-                        int temp, res = 0;
-                        for (int i = 0; i < 5; i++) {
-                            System.out.print("Введите число " + (i + 1) + ": ");
-                            temp = scanner.nextInt();
-                            res = funk.lastNumSum(temp, res);
-                        }
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Итого: " + res);
-                        break;
-                    }
                     default: {
                         System.out.println("Нет задачи в задании");
                         break;
                     }
                 }
-            }// Задача 4
+            }// Задача 7
+            case 8:{
+                System.out.println("-----------------------------------------------------------------------------------------");
+
+                break;
+            }// Задача 8
             default: {
                 System.out.println("нет в списке");
 
